5a68d1ce2aaa5ca9d2f18d94710305b767b60421
Waldemir Netto <79091320+wldmrntt@users.noreply.github.com>
2023-11-26 20:56:11 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,68 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, NgZone, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-calendario',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule],
+  templateUrl: './calendario.component.html',
+  styleUrl: './calendario.component.css'
 })
-export class AppComponent {
-  title = 'Pratica_10';
+
+export class CalendarioComponent implements OnInit {
+
+  dataAtual: Date = new Date();
+  diasCalendario: Date[] = [];
+  diaSelecionadoIndex!: number;
+  diaDaSemanaSelecionado: string = '';
+
+  constructor(private cdr: ChangeDetectorRef, private ngZone: NgZone) {}
+
+  ngOnInit() {
+    this.construirCalendario();
+  }
+
+  construirCalendario() {
+    const ano = this.dataAtual.getFullYear();
+    const mes = this.dataAtual.getMonth();
+
+    const primeiroDiaDaSemana = 0; // domingo
+    const ultimoDiaDaSemana = 6;   // sábado
+
+    const dataInicial = new Date(ano, mes, 1);
+    while (dataInicial.getDay() !== primeiroDiaDaSemana) {
+      dataInicial.setDate(dataInicial.getDate() - 1);
+    }
+
+    const dataFinal = new Date(ano, mes + 1, 0);
+    while (dataFinal.getDay() !== ultimoDiaDaSemana) {
+      dataFinal.setDate(dataFinal.getDate() + 1);
+    }
+    this.diasCalendario = [];
+    for (
+      let data = new Date(dataInicial.getTime());
+      data <= dataFinal;
+      data.setDate(data.getDate() + 1)
+    ) {
+      this.diasCalendario.push(new Date(data.getTime()));
+    }
+  }
+
+  alterarMes(offsetMes: number) {
+    this.dataAtual.setMonth(this.dataAtual.getMonth() + offsetMes);
+    this.dataAtual = new Date(this.dataAtual.getTime());
+    this.construirCalendario();
+  }
+
+  selecionarDiaSemana(index: number) {
+    const diasDaSemana = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
+    const diaSelecionado = this.diasCalendario[index].getDay();
+    this.diaDaSemanaSelecionado = diasDaSemana[diaSelecionado];
+
+    console.log('Dia da semana selecionado:', this.diaDaSemanaSelecionado);
+
+    this.ngZone.run(() => {
+      this.cdr.detectChanges();
+    });
+  }
 }
